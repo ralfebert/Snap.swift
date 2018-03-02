782c3305000991248309373588e1995a7d4db56c
Ralf Ebert <info@ralfebert.de>
2018-03-02 17:48:31 +0100

    only render using old UIGraphicsBeginImageContextWithOptions api, otherwise images will have different metadata

--- Snap/Core/Infrastructure/Extension/UIView/UIView+Render.swift
+++ Snap/Core/Infrastructure/Extension/UIView/UIView+Render.swift
@@ -32,24 +32,15 @@ extension UIView {
     // SpriteKit refuses to draw its hierarchy: render an image of the spritekit view and add it to the view to be able to capture it
     let addedViews = addImagesForRenderedViews(self)
     defer { addedViews.forEach { $0.removeFromSuperview() } }
-
-    if #available(iOS 10.0, *) {
-      let imageRendererFormat = UIGraphicsImageRendererFormat.default()
-      imageRendererFormat.opaque = true
-      let renderer = UIGraphicsImageRenderer(
-        bounds: bounds
-      )
-      return renderer.image { context in
-        layer.render(in: context.cgContext)
-      }
-    }
  
     defer {
       UIGraphicsEndImageContext()
     }
     UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
 
-    let context = UIGraphicsGetCurrentContext()!
+    guard let context = UIGraphicsGetCurrentContext() else {
+        return nil
+    }
     layer.render(in: context)
     return UIGraphicsGetImageFromCurrentImageContext()
   }
