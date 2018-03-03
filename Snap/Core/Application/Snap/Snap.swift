4a61cbb609faa592f2b58fd57eb562f2f9ef7dae
Ralf Ebert <info@ralfebert.de>
2018-03-03 11:30:27 +0100

    always enable ksdiff output

--- Snap/Core/Application/Snap/Snap.swift
+++ Snap/Core/Application/Snap/Snap.swift
@@ -6,5 +6,5 @@ public struct Snap {
 
 public final class Config {
   /// Enable Kaleidoscope ksdiff command line utils to show the difference between failed image and reference one
-  public var enableKaleidoscope = false
+  public var enableKaleidoscope = true
 }
